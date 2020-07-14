c51d5f8cbcab04b5219e1ff44de9d0d34f82b59b
Brandon Main <brandonmain@Brandons-MacBook-Air.local>
2020-07-13 19:35:05 -0700

    update

--- brandonmain-com/components/navbar.js
+++ brandonmain-com/components/navbar.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-//import styles from '../styles/navbar.module.css'
+import styles from '../styles/navbar.module.css'
 import { Component } from 'react'
 import classNames from 'classnames'
 
