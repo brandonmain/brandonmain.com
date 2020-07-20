3a621ee4dfa5388e73da168c66e9d1b84fd6762f
Brandon Main <brandonlmain@gmail.com>
2020-07-20 15:27:12 -0700

    added social

--- brandonmain-com/pages/index.js
+++ brandonmain-com/pages/index.js
@@ -1,6 +1,8 @@
 import styles from '../styles/index.module.css'
 import Head from 'next/head'
 import { Component } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faFacebook, faInstagram, faTiktok, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 class Home extends Component {
 
@@ -14,6 +16,16 @@ class Home extends Component {
       <img className={styles.headshot} src={'/img/bm.jpg'}/>
         <h1 className={styles.landing_name}>Brandon Main</h1>
         <p className={styles.landing_desc}>Full Stack Web and Mobile Software Engineer</p>
+        <ul className={styles.social_ul}>
+          <li><a href="https://twitter.com/brandonlmain"><FontAwesomeIcon className={styles.social} icon={faTwitter} /></a></li>
+          <li><a href="https://www.instagram.com/brandonmain/"><FontAwesomeIcon className={styles.social} icon={faInstagram} /></a></li>
+          <li><a href="https://www.facebook.com/brandon.main.7"><FontAwesomeIcon className={styles.social} icon={faFacebook} /></a></li>
+          <li><a href="https://www.tiktok.com/@sacredbeometry?lang=en"><FontAwesomeIcon className={styles.social} icon={faTiktok} /></a></li>
+          <li><a href="https://github.com/brandonmain"><FontAwesomeIcon className={styles.social} icon={faGithub} /></a></li>
+          <li><a href="https://www.linkedin.com/in/brandon-main/"><FontAwesomeIcon className={styles.social} icon={faLinkedin} /></a></li>
+        </ul>
+      </div>
+      <div className={styles.index_container2}>
       </div>
       </>
     )
