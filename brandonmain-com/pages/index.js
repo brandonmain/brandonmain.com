e74fc87e998b63a9a6862deb314a2d754cad5219
Brandon Main <brandonlmain@gmail.com>
2020-07-20 14:35:30 -0700

    addded responsivness on load

--- brandonmain-com/pages/index.js
+++ brandonmain-com/pages/index.js
@@ -11,8 +11,9 @@ class Home extends Component {
         <title>Brandon Main | Welcome</title>
       </Head>
       <div className={styles.index_container}>
+      <img className={styles.headshot} src={'/img/bm.jpg'}/>
         <h1 className={styles.landing_name}>Brandon Main</h1>
-        <p>Full Stack Web and Mobile Software Engineer</p>
+        <p className={styles.landing_desc}>Full Stack Web and Mobile Software Engineer</p>
       </div>
       </>
     )
